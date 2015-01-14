033166bdbd933f453a0e08e42e2886114e5d5704
Stephen Darlington <stephen@zx81.org.uk>
2015-01-14 17:22:26 +0000

    Refresh the calendar when we're asked to

--- Calendar Widget/TodayViewController.m
+++ Calendar Widget/TodayViewController.m
@@ -141,9 +141,9 @@
 }
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult result))completionHandler {
-    // Update your data and prepare for a snapshot. Call completion handler when you are done
-    // with NoData if nothing has changed or NewData if there is new data since the last
-    // time we called you
+    [self updateCalendar:self.displayDate];
+    [self updateDateLabel:self.displayDate];
+    
     completionHandler(NCUpdateResultNoData);
 }
 
